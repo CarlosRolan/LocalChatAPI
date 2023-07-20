82914c4b3afd10852e92a1d75fd96ce11b6a6afa
Carlos Rolán Díaz <carlosrolandiaz@gmail.com>
2023-07-20 13:02:46 +0200

    First commitor not?

--- src/com/Msg.java
+++ src/com/Msg.java
@@ -17,38 +17,22 @@ public class Msg implements Serializable {
     private String[] parameters = { "none" };
     private String body = "empty";
 
-    // Getters
-    /**
-     * @return the action
-     */
     public String getAction() {
         return action;
     }
 
-    /**
-     * @return the headers
-     */
     public String[] getHeaders() {
         return headers;
     }
 
-    /**
-     * @return the EMISOR which is always the 0 position at the array
-     */
     public String getEmisor() {
         return headers[0];
     }
 
-    /**
-     * @return the RECEPTOR which is always the 1 position at the array
-     */
     public String getReceptor() {
         return headers[1];
     }
 
-    /**
-     * @return the parameters
-     */
     public String[] getParameters() {
         return parameters;
     }
@@ -57,9 +41,6 @@ public class Msg implements Serializable {
         return parameters[pos];
     }
 
-    /**
-     * @return the body
-     */
     public String getBody() {
         return body;
     }
@@ -68,54 +49,31 @@ public class Msg implements Serializable {
         return getBody();
     }
 
-    // Setters
-    /**
-     * @param action the action to set
-     */
     public void setAction(String action) {
         this.action = action;
     }
 
-    /**
-     * @param headers the headers to set
-     */
     public void setHeaders(String[] headers) {
         this.headers = headers;
     }
 
-    /**
-     * @param headers the EMISOR at the 0 position
-     */
     public void setEmisor(String emisor) {
         headers[0] = emisor;
     }
 
-    /**
-     * @param headers the EMISOR at the 0 position
-     */
     public void setReceptor(String receptor) {
         headers[1] = receptor;
     }
 
-    /**
-     * @param parameters the parameters to set
-     */
     public void setParameters(String[] parameters) {
         this.parameters = parameters;
     }
 
-    /**
-     * @param pos       the position where whe setting the param
-     * @param parameter the parameter to set
-     */
     public void setParameter(int pos, String parameter) {
         this.parameters[pos] = parameter;
 
     }
 
-    /**
-     * @param body the body to set
-     */
     public void setBody(String body) {
         this.body = body;
     }
