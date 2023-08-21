62f05971d88adafa059258157af87faf7ab0177e
Carlos Rolán Díaz <carlosrolandiaz@gmail.com>
2023-08-21 11:19:53 +0200

    FIX

--- src/com/comunication/Connection.java
+++ src/com/comunication/Connection.java
@@ -250,7 +250,7 @@ public class Connection extends Thread implements ApiCodes {
 		System.out.println("RECONNECTED");
 	}
 
-	public void write(Object obj) {
+	public void write(Object obj) throws SocketException, IOException {
 		if (obj instanceof MSG) {
 			System.out.println("MSG_OUT==>" + obj.toString());
 			writeMessage((MSG) obj);
